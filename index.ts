375b6b7f900d7b328964e911ea97fc1f0d2eb8a6
Igor Savin <iselwin@gmail.com>
2023-10-01 14:05:07 +0300

    Implement better slicing methods

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 export { defaultLogger } from './src/logger'
 export { executeSyncChunksSequentially } from './src/butterSpread'
-export { chunk, splitString } from './src/arrayUtils'
+export { chunk } from './src/arrayUtils'
+export { splitTextPreserveWords, getSlicePreserveWords } from './src/stringUtils'
 
 export type { Logger, LogFn } from './src/logger'
 export type { ExecutionOptions, SyncProcessor } from './src/butterSpread'
